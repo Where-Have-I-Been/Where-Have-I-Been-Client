9ba1fbdcb00ec3928a2f248831c0e3660f8c5222
rewe999 <zymek998@gmail.com>
2020-12-28 19:51:22 +0100

    #37 - Design community panel

--- src/views/Community.vue
+++ src/views/Community.vue
@@ -1,13 +1,19 @@
 <template>
-  <h1>Community</h1>
+  <nav-bar></nav-bar>
+  <div class="container pt-3">
+    <c-tmp></c-tmp>
+  </div>
 </template>
 
 <script>
+import NavBar from "../components/NavBar.vue";
+import cTmp from "../components/Community/communityTemplate.vue";
+
 export default {
-    name: 'Community'
-}
+  name: "Community",
+  components: { NavBar, cTmp },
+};
 </script>
 
 <style>
-
 </style>
---
+++ src/views/CommunityuUser.vue
@@ -0,0 +1,19 @@
+<template>
+  <nav-bar></nav-bar>
+  <div class="container pt-3">
+    <c-u></c-u>
+  </div>
+</template>
+
+<script>
+import NavBar from "../components/NavBar.vue";
+import cU from "../components/Community/Users/communityUserTemplate.vue";
+
+export default {
+  name: "Community",
+  components: { NavBar, cU },
+};
+</script>
+
+<style>
+</style>
