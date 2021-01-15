11bf329a741388f0420f42e75003997f52298688
rewe999 <zymek998@gmail.com>
2021-01-15 13:41:47 +0100

    #62 - handle trips in profile

--- src/components/Community/cardTrip.vue
+++ src/components/Community/cardTrip.vue
@@ -1,17 +1,14 @@
 <template>
-  <div class="card mb-3">
-    <div class="row g-0">
+  <div class="card mb-3" v-for="(trip, i) in Trips" :key="i">
+    <div class="row g-0" v-if="Trips">
       <div class="col-md-4 jpg rounded mx-auto d-block">
-        <img src="../../assets/avatar2.jpg" alt="..." class="jpg" />
+        <img :src="trip.photo.url" alt="Trip image" class="jpg" />
       </div>
       <div class="col-md-8">
         <div class="card-body">
-          <h5 class="card-title">Card title</h5>
+          <h5 class="card-title">{{ trip.name }}</h5>
           <p class="card-text">
-            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Numquam ut
-            hic ipsum enim eaque laborum doloribus error consequatur nostrum,
-            perspiciatis voluptatem cumque distinctio quod illum quis
-            necessitatibus omnis ducimus totam.
+            {{ trip.description }}
           </p>
           <p class="card-text">
             <small class="text-muted">Last updated 3 mins ago</small>
@@ -23,9 +20,35 @@
 </template>
 
 <script>
+import axios from "axios";
+
 export default {
   name: "cardTrip",
+  data() {
+    return {
+      Trips: null,
+    };
+  },
   props: { trips: null },
+  methods: {
+    async getUserTrips() {
+      if (this.id == undefined) {
+        const user = await axios.get(
+          "trips/user/" + localStorage.getItem("userID"),
+          {
+            headers: {
+              Authorization: `Bearer ${localStorage.getItem("token")}`,
+            },
+          }
+        );
+        this.Trips = user.data.data;
+        console.log(user.data.data);
+      }
+    },
+  },
+  mounted() {
+    this.getUserTrips();
+  },
 };
 </script>
 
